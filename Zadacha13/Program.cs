0c9e8710ce6ea4220323d58b44988ac0a8cb77d8
Madina <sagdeeva.m.r@mail.ru>
2022-10-09 20:41:20 +0300

    Zadacha 10: output of the third digit of the number if it's possible

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,24 @@
+﻿/*13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+void FindThirdDigit()
+{
+    int newNumber = (new Random()).Next(1, 100000);
+    Console.WriteLine($"Исходное число: {newNumber}");
+    string numericAsSting = newNumber.ToString();
+
+if (newNumber > 99)
+{
+    int result = int.Parse (numericAsSting[2].ToString());
+    Console.WriteLine($"Третья цифра числа: {result}");
+}
+
+else 
+    Console.WriteLine($"Третьей цифры числа не существует");
+}
+
+FindThirdDigit();

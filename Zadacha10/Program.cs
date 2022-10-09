33bd8c323827fd1c0393308de731857d796ef512
Madina <sagdeeva.m.r@mail.ru>
2022-10-09 19:48:52 +0300

    Zadacha 10: output of the second digit of the number

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,17 @@
+﻿/*10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+void FindSecondDigit()
+{
+    Console.WriteLine("Введите трёхзначное число:");
+    string newNumber = Console.ReadLine();
+    string numericAsSting = newNumber.ToString();
+    int result = int.Parse (numericAsSting[1].ToString());
+    Console.WriteLine($"Result number: {result}");
+}
+
+FindSecondDigit();
